c6f6735e76f218d302682f5a315714b0a48f1e0b
Oláh <adam.olah06@gmail.com>
2026-05-22 17:13:04 +0200

    Base universal propertes added

--- main.py
+++ main.py
@@ -14,3 +14,29 @@ import time
 import sys
 import re
 import os
+
+MAX_WORKERS = 35
+MAX_RESULTS = 4
+TIMEOUT = 4
+
+EMAIL_REGEX = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+PROJECT_REGEX = re.compile(r"\d{4}-\d-[A-Z]{2}\d{2}-KA1\d{2}-[A-Z]{3}-\d{5,}")
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+REQUIRED_PACKAGES = {
+    "pandas": "pandas",
+    "requests": "requests",
+    "xlsxwriter": "xlsxwriter",
+    "ddgs": "ddgs",
+    "questionary": "questionary",
+    "openpyxl": "openpyxl",
+}
+
+pd = None
+requests = None
+xlsxwriter = None
+DDGS = None
+questionary = None
